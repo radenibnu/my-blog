e69b79f31608161256f38ce1af6a1765aa869f63
radenibnu <radenibnuqc@gmail.com>
2025-04-23 12:35:32 +0700

    Fix Comment & Like

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -6,16 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    protected $fillable = ['user_id', 'post_id'];
+    protected $fillable = [
+        'post_id',
+        'user_id',
+    ];
 
     public function post()
     {
         return $this->belongsTo(Post::class);
     }
-    
+
     public function user()
     {
         return $this->belongsTo(User::class);
     }
-    
 }
